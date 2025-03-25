6974c3004a026a9880614a7c1a82f11f9d17bd93
Cuku <xhikab@gmail.com>
2025-03-26 00:16:09 +0100

    Load UIConfig

--- Source/InfiniTwin/Private/UI/UIITModule.cpp
+++ Source/InfiniTwin/Private/UI/UIITModule.cpp
@@ -9,37 +9,39 @@
 #endif
 
 namespace UI {
-    UIITModule::UIITModule(flecs::world& world) {
-        world.module<UIITModule>();
+	UIITModule::UIITModule(flecs::world& world) {
+		world.module<UIITModule>();
 
-        Initialize(world);
-    }
+		Initialize(world);
+	}
 
-    void UIITModule::Initialize(flecs::world& worl) {
-        // Setup main viewport window
-        if (UGameUserSettings* Settings = GEngine->GameUserSettings)
-        {
-            // 1) Calculate window size minus title bar
+	void UIITModule::Initialize(flecs::world& worl) {
+#if !WITH_EDITOR
+		// Setup main viewport window
+		if (UGameUserSettings* Settings = GEngine->GameUserSettings)
+		{
+			// 1) Calculate window size minus title bar
 #if PLATFORM_WINDOWS
-            const int32 CaptionHeight = ::GetSystemMetrics(SM_CYCAPTION);
-            const int32 FrameThickness = ::GetSystemMetrics(SM_CYSIZEFRAME);
-            const int32 TitleBarHeight = CaptionHeight + FrameThickness;
+			const int32 CaptionHeight = ::GetSystemMetrics(SM_CYCAPTION);
+			const int32 FrameThickness = ::GetSystemMetrics(SM_CYSIZEFRAME);
+			const int32 TitleBarHeight = CaptionHeight + FrameThickness;
 #else
-            const int32 TitleBarHeight = 50;
+			const int32 TitleBarHeight = 50;
 #endif
 
-            FIntPoint DesktopRes = Settings->GetDesktopResolution();
-            const int32 DesiredWidth = DesktopRes.X;
-            const int32 DesiredHeight = DesktopRes.Y - TitleBarHeight;
+			FIntPoint DesktopRes = Settings->GetDesktopResolution();
+			const int32 DesiredWidth = DesktopRes.X;
+			const int32 DesiredHeight = DesktopRes.Y - TitleBarHeight;
 
-            Settings->SetFullscreenMode(EWindowMode::Windowed);
-            Settings->SetScreenResolution(FIntPoint(DesiredWidth, DesiredHeight));
-            Settings->ApplySettings(false);
+			Settings->SetFullscreenMode(EWindowMode::Windowed);
+			Settings->SetScreenResolution(FIntPoint(DesiredWidth, DesiredHeight));
+			Settings->ApplySettings(false);
 
-            // 2) After the viewport is set, move the window via Slate
-            TSharedPtr<SWindow> MainWindow = FSlateApplication::Get().GetActiveTopLevelWindow();
-            if (MainWindow.IsValid())
-                MainWindow->MoveWindowTo(FVector2D(0, TitleBarHeight));
-        }
-    }
+			// 2) After the viewport is set, move the window via Slate
+			TSharedPtr<SWindow> MainWindow = FSlateApplication::Get().GetActiveTopLevelWindow();
+			if (MainWindow.IsValid())
+				MainWindow->MoveWindowTo(FVector2D(0, TitleBarHeight));
+		}
+#endif
+	}
 }
