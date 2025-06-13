d6c33eeb3fa034ce5f9fb7fc831cd5cd0d7f03aa
Cuku <xhikab@gmail.com>
2025-06-13 16:56:17 +0200

    Refactor

--- Source/InfiniTwin/Private/UI/ITUI.cpp
+++ Source/InfiniTwin/Private/UI/ITUI.cpp
@@ -4,17 +4,12 @@
 #include "UI/ITUI.h"
 #include "UIElements.h"
 #include "UI/ITUIFeature.h"
-#include "UI/ITUIWidgetFeature.h"
 
 namespace UI{
 	ITUI::ITUI(flecs::world& world) {
 		world.module<ITUI>();
 
 		UI::Register(world);
-
-		ITUIWidgetFeature::RegisterComponents(world);
-
-		//ITUIWidgetFeature::CreateObservers(world);
 
 		ITUIFeature::Initialize(world);
 	}
